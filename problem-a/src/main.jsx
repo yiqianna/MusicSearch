cca4db9e2bb7a96a223fb23fed133c2db292cd8e
Anna Huang <anna1018@uw.edu>
2026-03-25 23:27:34 -0700

    Initial commit: MusicSearch project

---
+++ problem-a/src/main.jsx
@@ -0,0 +1,16 @@
+import 'whatwg-fetch';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App';
+import { BrowserRouter } from 'react-router';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style.css'; //import css file!
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
